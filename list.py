2f7b12ab006b61027576f5947be07a9801c42c66
Roykiprotich <roykiprotich065@gmail.com>
2025-08-16 18:59:01 +0300

    Update list.py

--- list.py
+++ list.py
@@ -1,17 +1,17 @@
 # Create an empty list
-my_list = [10, 20, 30, 40 ]
+my_list = [ ]
 
 # Append elements 10, 20, 30, 40
-my_list.append(50)
-my_list.append(60)
-my_list.append(70)
-my_list.append(80)
+my_list.append(10)
+my_list.append20)
+my_list.append(30)
+my_list.append(40)
 
 # Insert value 15 at the second position (index 1)
 my_list.insert(1, 15)
 
 # Extend my_list with another list [50, 60, 70]
-my_list.extend([90, 95, 99])
+my_list.extend([50, 60, 70])
 
 # Remove the last element
 my_list.pop()
